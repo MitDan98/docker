4555b8831558bcc5d9f77f4d8085f5d9bd0a8556
MitDan98 <56722622+MitDan98@users.noreply.github.com>
2020-01-06 21:31:04 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,26 +1,85 @@
 #!/bin/bash
 #DOCKER Install 
 #Easy way
+#Update system and install docker
 sus = $?
-cat /etc/os-release
-if [ -r "cat /etc/os-release CentOS" ]; then
-        sudo yum update
-          if [ [ $sus -ne 0 ] ; then 
-              echo " try agian... "
-              exit 1
-          else 
-            echo "Excelent"
-         fi
-elif [ -r "Ubuntu" or "Debian" ] ; then 
-        sudo apt-get update
-          if [ [ $sus -ne 0 ] ] ; then 
-              echo " try agian... "
-              exit 1
-          else 
-            echo "Excelent"
-         fi
-else
-      echo "Can't not found..."
-      exit 1
+sudo yum update
+sudo yum install -y yum-utils device-mapper-persistent-data lvm2
+if [ $sus -ne 0 ]; then 
+     echo " Docker was install already... "
+     exit 1
+else 
+     echo "Docker has been installed with succes..."
 fi
+#
+#
+#Add docker repo
+sudo yum-config-manager --add-repo https://download.docker.com/linux/centos/docker-ce.repo
+if [ $sus -ne 0 ]; then 
+       echo " This repo it was add already "
+       exit1
+else 
+       echo "Repo has been add with succes..."
+fi
+#
+#
+#Install docker-ce and enable
+sudo yum install docker-ce docker-ce-cli containerd.io
+if [ $sus -ne 0 ]; then 
+     echo " Docker-ce was install already... "
+     exit 1
+else 
+     echo "Docker-ce has been installed with succes..."
+fi
+sudo yum-config-manager --enable docker-ce-nightly
+if [ $sus -ne 0 ]; then 
+     echo " Docker-ce was enable already... "
+     exit 1
+else 
+     echo "Docker-ce has been enable with succes..."
+fi
+sudo yum-config-manager --enable docker-ce-test
+if [ $sus -ne 0 ]; then 
+     echo " Docker-ce was enable already... "
+     exti 1
+else 
+     echo "Docker-ce has been enable with succes..."
+fi 
+#
+#
+#
+#Adding Docker group and user
+echo "Adding Docker user and group"
+unseradd Dockeruser
+groupadd Docker
+usermod -a -G Docker Dockeruser
+#
+#
+#We start and test docker
+sudo systemctl enable docker.service
+sudo systemctl start docker.service
+sudo systemctl status docker.service
+#
+#
+# We install  Docker compose and install python pip
+sudo yum install -y python-pip
+sus = $?
+if [ $sus -ne 0 ]; then
+      echo " Python was installed already "
+      exit 1
+else 
+       echo " Python pip has been installed with succes"
+fi  
+sudo pip install docker-compose
+ if [ $sus -ne 0 ]; then
+      echo " docker-compose was installed already "
+      exit 1
+else 
+       echo " docker-compose pip has been installed with succes"
+       sudo yum update python*
+fi
+sudo docker run hello-world
+echo " Docker has been install with succes"
+
+
 
